ed17ca55a5fae254ae89efeab7c2d366c666b5c1
Supriya <49004629+supriyavk@users.noreply.github.com>
2022-09-13 18:59:28 +0200

    Removed println statement

--- src/main/kotlin/com/iptiq/domain/Provider.kt
+++ src/main/kotlin/com/iptiq/domain/Provider.kt
@@ -1,5 +1,6 @@
 package com.iptiq.domain
 
+
 data class Provider(
     val id: Int,
     var heartBeatSuccessCount: Int = 0,
@@ -7,9 +8,9 @@ data class Provider(
     var isEnabled: Boolean = true
 ) {
     var checkFlag: Boolean = true
+    
     fun get() = this.id.toString()
     fun check(): Boolean {
-        println("Executing provider check")
         return checkFlag
     }
 }
