eaa85611969ab6fb793cf7db96b979a668c60c6e
Supriya <riya.kotharkar@gmail.com>
2022-09-13 16:24:45 +0200

    Added logger

--- src/main/kotlin/com/iptiq/domain/Provider.kt
+++ src/main/kotlin/com/iptiq/domain/Provider.kt
@@ -1,15 +1,18 @@
 package com.iptiq.domain
 
+import com.sun.org.slf4j.internal.LoggerFactory
+
 data class Provider(
     val id: Int,
     var heartBeatSuccessCount: Int = 0,
     var isAlive: Boolean = true,
     var isEnabled: Boolean = true
 ) {
+    private val logger = LoggerFactory.getLogger(Provider::class.java)
     var checkFlag: Boolean = true
     fun get() = this.id.toString()
     fun check(): Boolean {
-        println("Executing provider check ")
+        logger.debug("Executing provider check")
         return checkFlag
     }
 }
